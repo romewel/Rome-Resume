1cac5e9d7c8b685530a05d656b948b030e77a95d
romewel <133300205+romewel@users.noreply.github.com>
2024-04-22 09:48:43 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings: ["FrontEnd Developer", "IT Specialist", "Data Entry"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
